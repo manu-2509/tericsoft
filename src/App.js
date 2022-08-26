5f8c05e3c9ac6684d3f140cbf6578a7b9c0b10a7
manu-2509 <maneshs111@gmail.com>
2022-08-26 18:44:35 +0530

    highlight

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
-import './App.css';
 import {Header} from './components/header'
+import { Bar } from './components/Bar';
+import './App.css';
+
 function App() {
   return (
     <div className="App">
    <Header></Header>
+   <Bar></Bar>
     </div>
   );
 }
--- src/components/Bar.jsx
+++ src/components/Bar.jsx
@@ -1,10 +1,11 @@
+import "./style.css"
 export const Bar=()=>{
     return(
-       <div>
-        <p>BMX</p>
-        <p>Mountain</p>
-        <p>Road</p>
-        <p>Track</p>
+       <div className="bar">
+        <p className="bar-part part1">BMX</p>
+        <p className="bar-part part2">Mountain</p>
+        <p className="bar-part part3">Road</p>
+        <p className="bar-part part4">Track</p>
        </div>
     )
 }
